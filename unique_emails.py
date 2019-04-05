1c23ece9d1213cb9f1aefbad909411de58be73a4
John Goure <johngoure@protonmail.com>
2019-04-04 21:22:01 -0500

    fixed to adhere to pep-9

--- unique_emails.py
+++ unique_emails.py
@@ -1,15 +1,18 @@
 import re
 
 
-def numUniqueEmails(emails: [str]) -> int:
-    
+def numUniqueEmails(emails: [str]) -> int:    
     return len(set(map(parse, emails)))
+
 
 def parse(email):
         local, domain = email.split('@')
         local = local.split('+')[0].replace('.', '')
         return f"{local}@{domain}"
 
-mail = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+mail = [
+    "test.email+alex@leetcode.com",
+    "test.e.mail+bob.cathy@leetcode.com",
+    "testemail+david@lee.tcode.com"]
 
 print(numUniqueEmails(mail))
