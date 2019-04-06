ee15e7dd1590ea135c1688a38a03060fd419dedb
John Goure <johngoure@protonmail.com>
2019-04-05 21:14:37 -0500

    Added a main wrapper. So i could use this module and not have to run the console input.

--- unique_characters.py
+++ unique_characters.py
@@ -8,10 +8,11 @@ def is_unique(word: str) -> bool:
 
     return True
 
-user_input = input(
-    "Please enter a word to check if it has unique characters: "
+if __name__ == "__main__":
+    user_input = input(
+        "Please enter a word to check if it has unique characters: "
     )
-if (is_unique(user_input)):
-    print(f"{user_input} is full of unique characters.")
-else:
-    print(f"{user_input} has matching characters.")
+    if (is_unique(user_input)):
+        print(f"{user_input} is full of unique characters.")
+    else:
+        print(f"{user_input} has matching characters.")
