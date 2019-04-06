47cb377f475eee1380eee11f7f70ac4677a7eff4
John Goure <johngoure@protonmail.com>
2019-04-05 21:09:25 -0500

    solved problem 1.1 in CTCI. Used a key/value map. Runs in linear time because the program has to check every character.

---
+++ unique_characters.py
@@ -0,0 +1,17 @@
+def is_unique(word: str) -> bool:
+    wordCount = {}
+    for letter in word:
+        if wordCount.__contains__(letter):
+            return False
+        else:
+            wordCount[letter] = 1
+
+    return True
+
+user_input = input(
+    "Please enter a word to check if it has unique characters: "
+    )
+if (is_unique(user_input)):
+    print(f"{user_input} is full of unique characters.")
+else:
+    print(f"{user_input} has matching characters.")
