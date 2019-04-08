f93afa80359214e00ff1adcefdaadd99b7dab561
John Goure <johngoure@protonmail.com>
2019-04-07 20:31:33 -0500

    Fixed issue missing last letter if last letter count is 1.

--- word_compression.py
+++ word_compression.py
@@ -1,15 +1,20 @@
 def compress_word(word):
-    compressed_word = ""
+    compressed_word = f"{word[0]}"
     count = 1
-    for i in range(0, len(word) - 1):
-        if word[i + 1] == word[i]:
-            count += 1
-        if word[i] != word[i + 1] or i + 2 == len(word):
-            compressed_word += f"{word[i]}{count}"
-            count = 1
+    # loop through the letters in the word
+    for i in range(1, len(word)):
+
+        if word[i - 1] != word[i]:
+            compressed_word += f"{count}{word[i]}"
+            count = 0
+        if i == len(word) - 1:
+            compressed_word += f"{count + 1}"
+        count += 1
+    # If the compressed word is the same length as the
+    # original word, return the original word.
     if len(compressed_word) == len(word):
         return word
     return compressed_word
 
-test = "aaa"
+test = "aaaaaabbbcdefffghjklmnopqrstuvwxyz"
 print(compress_word(test))
