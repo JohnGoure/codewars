6596c7c05a07857acb56c03f0bec3482f11db237
John Goure <johngoure@protonmail.com>
2019-04-13 16:47:21 -0500

    added a test for next, node type, next type, and bad input.

--- test_queue.py
+++ test_queue.py
@@ -17,3 +17,23 @@ def test_print_queue():
     for x in range(100):
         mockResult.queue(x)
     assert str(mockResult) == str(mockTest)
+
+def test_queue_next():
+    mock = MyQueue()
+    mock.queue(2)
+    mock.queue(3)
+    assert mock.first.next.key == 3
+
+def test_queue_node():
+    mock = MyQueue()
+    mock.queue(2)
+    assert type(mock.first) == MyQueue._Node
+
+def test_queue_node_next_type():
+    mock = MyQueue()
+    mock.queue(2)
+    assert mock.first.next == None
+
+def test_queue_bad_input():
+    mock = MyQueue()
+    assert mock.queue(None) == "Can not enqueue None"
