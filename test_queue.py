47ff737a8b69ea11d4d4c12113cef4de940a48e3
John Goure <johngoure@protonmail.com>
2019-04-13 16:02:25 -0500

    implemented 2 more tests.

--- test_queue.py
+++ test_queue.py
@@ -1,7 +1,19 @@
-from queue import Queue
+from myqueue import MyQueue
 
-def test_queue():
-    mock_queue = Queue()
+def test_queue_deque():
+    mock_queue = MyQueue()
     mock_queue.queue(2)
     mock_queue.queue(3)
-    assert mock_queue.dequeue().key == 2
+    assert mock_queue.dequeue() == 2
+
+def test_queue_queue():
+    mock = MyQueue()
+    mock.queue(2)
+    assert mock.first.key == 2
+
+def test_print_queue():
+    mockResult = MyQueue()
+    mockTest = [x for x in range(100)]
+    for x in range(100):
+        mockResult.queue(x)
+    assert str(mockResult) == str(mockTest)
