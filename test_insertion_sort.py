251df22ad902510022252972c43da94fb07bc2ba
John Goure <johngoure@protonmail.com>
2019-04-14 13:23:43 -0500

    implemented a few tests.

---
+++ test_insertion_sort.py
@@ -0,0 +1,17 @@
+import insertion_sort
+
+def test():
+    mock = [4,3,2]
+    assert insertion_sort.insertion_sort(mock) == sorted(mock)
+
+def test_empty_list():
+    mock = []
+    assert insertion_sort.insertion_sort(mock) == mock
+
+def test_single_item():
+    mock = [1]
+    assert insertion_sort.insertion_sort(mock) == mock
+
+def test_mixed_item():
+    mock = [1,'a']
+    assert insertion_sort.insertion_sort(mock) == TypeError
