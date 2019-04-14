462f334f60c3215129fc9da82a59225a4def233b
John Goure <johngoure@protonmail.com>
2019-04-14 13:59:01 -0500

    Added a catch to the sort to return an error if there is an exception.

--- Sorts/insertion_sort.py
+++ Sorts/insertion_sort.py
@@ -1,9 +1,12 @@
 def insertion_sort(arr):
     for x in range(0, len(arr)):
         i = x
-        while i > 0 and arr[i - 1] > arr[i]:
-            arr[i], arr[i - 1] = arr[i - 1], arr[i]
-            i -= 1
+        try:            
+            while i > 0 and arr[i - 1] > arr[i]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                i -= 1
+        except:
+            return Exception
     return arr
 
 mock = [3, 2, 1, 7, 8, 9, 10, 6, 5, 4]
