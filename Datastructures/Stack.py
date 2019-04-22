ccef5993dc72aed308b4a7628a093993f1d09379
John Goure <johngoure@protonmail.com>
2019-04-22 18:05:49 -0500

    Implemented pop functionality.

--- Datastructures/Stack.py
+++ Datastructures/Stack.py
@@ -7,5 +7,10 @@ class Stack:
         self.arr.append(item)
         self.top += 1
 
+    def pop(self):
+        item = self.arr[self.top]
+        self.top -= 1
+        return item
+
     def peek(self):
         return self.arr[self.top]
