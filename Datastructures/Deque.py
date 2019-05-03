274a666b53abbed975ff18e54a2718d6c2b15bf5
John Goure <johngoure@protonmail.com>
2019-05-02 20:39:47 -0500

    Finished implementing add_first and first for the test.

--- Datastructures/Deque.py
+++ Datastructures/Deque.py
@@ -9,8 +9,15 @@ class Deque():
         self._front = 0
 
     def add_first(self, e):
+        """Add an item to the from the queue."""
         if (self._size == len(self._arr)):
             self._resize(2 * len(self._arr))
+        self._arr[(self._front - 1) % len(self._arr)] = e
+        self._front -= 1
+        self._size += 1
+
+    def first(self):
+        return self._arr[self._front]
 
     def _resize(self, c):
         old = self._arr
