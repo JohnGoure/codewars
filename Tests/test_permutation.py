972dcee7fc87b93ba794639deb43a55b3bf4860e
John Goure <johngoure@protonmail.com>
2019-05-09 21:01:06 -0500

    Added more tests.

--- Tests/test_permutation.py
+++ Tests/test_permutation.py
@@ -2,5 +2,18 @@ from Solutions.Permutation import permutation
 
 
 def test_permutation():
-    """Check if one word is a peprmutation of another word"""
+    """Check if one word is a permutation of another word"""
     assert permutation("dog", "god") == True
+
+
+def test_non_permutation():
+    """Check if one word is not a permutation of another word"""
+    assert permutation("dog", "cat") == False
+
+
+def test_diff_length():
+    assert permutation("howdy", "more") == False
+
+
+def test_longer_permutation():
+    assert permutation("Cowpoke", "powokce") == True
