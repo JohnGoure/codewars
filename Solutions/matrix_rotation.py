9269937ab8320c337f277cc7d36c89f31fa94f92
John Goure <johngoure@protonmail.com>
2019-05-12 11:44:19 -0500

    added depth and height methods.

--- Solutions/matrix_rotation.py
+++ Solutions/matrix_rotation.py
@@ -1,6 +1,6 @@
 def rotate(matrix):
     """Rotate an N X N matrix 90 degrees to the left."""
-    for x in range(len(matrix)):
+    for x in range(len(matrix) - 1, 0, -1):
         for y in range(len(matrix)):
-            matrix[y][x] = matrix[x][x]
+            matrix[x][y] = matrix[x][x]
     return matrix
--- Tests/test_matrix_rotation.py
+++ Tests/test_matrix_rotation.py
@@ -4,9 +4,9 @@ from Solutions.matrix_rotation import rotate
 def test_rotation():
     assert rotate([['a']*4, ['b']*4, ['c']*4, ['d']*4]) == (
         [
-            ['a', 'b', 'c', 'd'],
-            ['a', 'b', 'c', 'd'],
-            ['a', 'b', 'c', 'd'],
-            ['a', 'b', 'c', 'd']
+            ['d', 'c', 'b', 'a'],
+            ['d', 'c', 'b', 'a'],
+            ['d', 'c', 'b', 'a'],
+            ['d', 'c', 'b', 'a']
         ]
     )
