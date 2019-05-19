1c39089cac602840c1b022c47b5e768e102d4711
John Goure <johngoure@protonmail.com>
2019-05-19 14:02:24 -0500

    Updated the method arguments.

--- Tests/test_quicksort.py
+++ Tests/test_quicksort.py
@@ -3,5 +3,5 @@ from Sorts.quicksort import quicksort
 
 def test_sort():
     arr = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    quicksort(arr)
+    quicksort(arr, 0, len(arr) - 1)
     assert arr == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
