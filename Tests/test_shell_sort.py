e73fbf5791407ca5b041882c256cd997929a49ae
John Goure <johngoure@protonmail.com>
2019-05-19 13:59:28 -0500

    Updated method name.

--- Tests/test_shell_sort.py
+++ Tests/test_shell_sort.py
@@ -1,5 +1,6 @@
 import Sorts.shell_sort as ShellSort
 
+
 def test_sort():
     mock = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    assert ShellSort.sort(mock) == sorted(mock)
+    assert ShellSort.shellsort(mock) == sorted(mock)
