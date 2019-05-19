7754abb7b45b83d3ce277229a49051ef9fcfda5f
John Goure <johngoure@protonmail.com>
2019-05-19 14:10:50 -0500

    finished implementing quicksort.

--- Sorts/quicksort.py
+++ Sorts/quicksort.py
@@ -1,10 +1,20 @@
 def quicksort(arr, lo, hi):
+    """Sort the collection in O(NlgN) time."""
     if lo < hi:
         pivot = _partition(arr, lo, hi)
 
         quicksort(arr, lo, pivot - 1)
-        quicksort(arr, pivot + 1, high)
+        quicksort(arr, pivot + 1, hi)
 
 
 def _partition(arr, lo, hi):
-    raise NotImplementedError("Needs to be implemented")
+    """Place the pivot element at its correct position in the sorted array."""
+    i = lo - 1
+    pivot = arr[hi]
+
+    for j in range(lo, hi):
+        if arr[j] <= pivot:
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[hi] = arr[hi], arr[i+1]
+    return i+1
