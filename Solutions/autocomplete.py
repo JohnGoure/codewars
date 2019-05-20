339b8fd374fa3fd7ae77f3cd487ac3ad9200cc83
John Goure <johngoure@protonmail.com>
2019-05-19 19:37:07 -0500

    Implemented an autocomplete function.

---
+++ Solutions/autocomplete.py
@@ -0,0 +1,13 @@
+def autocomplete(input_, dictionary):
+    suggestions = []
+    input_ = ''.join(letters for letters in input_ if letters.isalpha())
+    for word in dictionary:
+        if len(suggestions) == 5:
+            return suggestions
+        if word[:len(input_)].lower() == input_.lower():
+            suggestions.append(word)
+    return suggestions
+
+if __name__ == '__main__':
+    dic = ["dog", "cat", "door", "damage"]
+    print(autocomplete("da$", dic))
